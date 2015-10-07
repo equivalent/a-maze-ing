97a843345ec93e3f2f760ef08cf07d1642b11eaa
Tomas Valent <equivalent@eq8.eu>
2015-10-07 17:37:45 +0000

    maze representation decorators

---
+++ spec/decorators/edge_decorator_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe EdgeDecorator, type: :view do
+  let(:maze_rep_object) { MazeMagic::Edge.instance }
+  subject { described_class.new(maze_rep_object) }
+
+  describe '#to_html' do
+    it_behaves_like 'maze_magic to_html div'
+    it do
+      expect(subject.to_html).to have_selector "div.edge"
+    end
+  end
+end
